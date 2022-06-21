3ad3b409356cacd677542fc7b199c0caae63bc71
Josh Josh <jmt0494@gmail.com>
2022-06-21 11:59:57 -0500

    added login functionality to employee service layer

---
+++ src/main/java/com/revature/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.revature.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.revature.dao.EmployeeDao;
+import com.revature.models.Employee;
+
+public class EmployeeService {
+
+	private EmployeeDao edao;
+	
+	public EmployeeService(EmployeeDao edao) {
+		this.edao = edao;
+	}
+	
+	public Employee confirmedLogin(String username, String password) {
+		
+		Optional<Employee> possibleEmp = edao.findAll().stream()
+				.filter(e -> (e.getUsername().equals(username) && e.getPassword().equals(password)))
+				.findFirst();
+		
+		return (possibleEmp.isPresent() ? possibleEmp.get() : new Employee());
+		
+	}
+	
+	public List<Employee> getAll() {
+		return edao.findAll();
+	}
+	
+}
